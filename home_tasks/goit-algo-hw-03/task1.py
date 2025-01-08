52a29840ddfb564c9cf75c113420c51dbaeacbca
Bohdan Razhyk <>
2025-01-08 12:23:33 +0100

    added ht

---
+++ home_tasks/goit-algo-hw-03/task1.py
@@ -0,0 +1,36 @@
+﻿from pathlib import Path
+
+
+def main():
+    try:
+        copy_files("Source", "Destination")
+    except Exception as e:
+        print(e)
+
+    print("done")
+
+def copy_files(source, dest="dist"):
+    path_source = Path(source)
+
+    if not path_source.exists():
+        raise Exception(f"path: {path_source} not exists")
+
+    for i in path_source.iterdir():
+        if i.is_dir():
+            copy_files(i, dest)
+        else:
+            new_file_path = Path(dest / Path(i.suffix.replace(".", "")))
+
+            if not Path(dest).exists():
+                Path(dest).mkdir()
+
+            if not new_file_path.exists():
+                new_file_path.mkdir()
+
+            new_file = Path(new_file_path / Path(i.name))
+
+            new_file.write_bytes(i.read_bytes())
+
+
+if __name__ == "__main__":
+    main()
