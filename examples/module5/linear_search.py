01154346a25ad9a7520c08785a38d4b0e93c8045
Bohdan Razhyk <>
2025-01-13 11:15:56 +0100

    added ex

---
+++ examples/module5/linear_search.py
@@ -0,0 +1,15 @@
+﻿def linear_search(arr, x):
+   for i in range(len(arr)):
+      if arr[i] == x:
+         return i
+   return -1
+
+
+def exists_in_list(arr, x):
+   return linear_search(arr, x) != -1
+
+
+numbers = [1, 3, 5, 7, 9, 11]
+print(linear_search(numbers, 7))  # Output: 3
+print(exists_in_list(numbers, 7))  # Output: True
+print(exists_in_list(numbers, 2))  # Output: False
